d051d89b6f751e92179159ff477090f2bb28d2df
Sachin <waghmaresachin124@gmail.com>
2024-08-26 23:40:40 +0530

    Corrected the mistake in constant

--- src/main/java/com/qa/opencart/contants/AppContants.java
+++ src/main/java/com/qa/opencart/contants/AppContants.java
@@ -2,7 +2,7 @@ package com.qa.opencart.contants;
 
 public class AppContants {
 
-    public static final String LOGIN_PAGE_TITLE = "Account Login11";
+    public static final String LOGIN_PAGE_TITLE = "Account Login";
 
     public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 
